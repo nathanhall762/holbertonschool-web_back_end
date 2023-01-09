6437b58f565964baefe0f3b4d4de135f103b9b16
nathanhall762 <4943@holbertonstudents.com>
2023-01-09 12:08:22 -0600

    task 12

---
+++ 0x00-ES6_basic/12-createReportObject.js
@@ -0,0 +1,9 @@
+export default function createReportObject(employeesList) {
+  const allEmployees = { ...employeesList };
+  return {
+    allEmployees,
+    getNumberOfDepartments() {
+      return Object.keys(allEmployees).length;
+    },
+  };
+}
