fcf2c2a8ae1cc929096a051a1c2fcfd8624daca7
nathanhall762 <4943@holbertonstudents.com>
2023-01-10 15:43:44 -0600

    eslint task 3

--- 0x02-ES6_classes/3-currency.js
+++ 0x02-ES6_classes/3-currency.js
@@ -7,15 +7,19 @@ export default class Currency {
   get code() {
     return this._code;
   }
+
   set code(value) {
     this._code = value;
   }
+
   get name() {
     return this._name;
   }
+
   set name(value) {
     this._name = value;
   }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`;
   }
