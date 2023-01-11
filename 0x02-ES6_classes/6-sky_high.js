63e657d9803f564257a10f4ba6e4be7fec447724
nathanhall762 <4943@holbertonstudents.com>
2023-01-11 13:41:00 -0600

    task 6

---
+++ 0x02-ES6_classes/6-sky_high.js
@@ -0,0 +1,24 @@
+import Building from './5-building';
+
+export default class SkyHighBuilding extends Building {
+  constructor(sqft, floors) {
+    super(sqft);
+    this.floors = floors;
+  }
+
+  get floors() {
+    return this._floors;
+  }
+
+  set floors(newFloors) {
+    if (typeof newFloors !== 'number') {
+      throw new TypeError('Floors must be a number');
+    } else {
+      this._floors = newFloors;
+    }
+  }
+
+  evacuationWarningMessage() {
+    return `Evacuate slowly the ${this.floors} floors`;
+  }
+}
