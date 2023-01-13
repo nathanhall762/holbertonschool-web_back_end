f766aa19d3df2fe7c709787b335a6cd5a814c402
nathanhall762 <4943@holbertonstudents.com>
2023-01-13 15:32:18 -0600

    fix 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,6 +1,9 @@
 /* eslint-disable no-unused-vars */
 export default function cleanSet(set, startString) {
   let cleanString = '';
+  if (startString === '') {
+    return cleanString
+  }
   set.forEach((value) => {
     if (value.startsWith(startString)) {
       cleanString += `${value.slice(startString.length)}-`;
