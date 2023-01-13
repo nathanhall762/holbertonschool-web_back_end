33b9d0e6e9017e78bdc8dd0b245829676e4a3aaa
nathanhall762 <4943@holbertonstudents.com>
2023-01-13 13:46:32 -0600

    style 5

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-vars */
 function createInt8TypedArray(length, position, value) {
-  if (position >= length || position < 0) {
-    throw new Error('Position outside range');
-  }
+//   if (position >= length || position < 0) {
+//     throw new Error('Position outside range');
+//   }
 
   const buffer = new ArrayBuffer(length);
   const int8View = new Int8Array(buffer);
