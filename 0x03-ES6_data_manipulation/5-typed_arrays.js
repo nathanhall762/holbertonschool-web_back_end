d38187ab2d24989f1b64b57df90d95a649c44a67
nathanhall762 <4943@holbertonstudents.com>
2023-01-13 13:40:01 -0600

    task 5

---
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -0,0 +1,10 @@
+function createInt8TypedArray(length, position, value) {
+  if (position >= length || position < 0) {
+    throw new Error('Position outside range');
+  }
+
+  const buffer = new ArrayBuffer(length);
+  const int8View = new Int8Array(buffer);
+  int8View[position] = value;
+  return buffer;
+}
