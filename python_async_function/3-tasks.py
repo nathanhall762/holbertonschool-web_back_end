fd1470eab2428dd83715279fc285b52f9baca255
nathanhall762 <4943@holbertonstudents.com>
2023-05-11 10:18:11 -0500

    docstring 3 and 4

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -12,13 +12,17 @@ wait_random = __import__('0-basic_async_syntax').wait_random
 def task_wait_random(max_delay: int) -> Any:
     """task_wait_random(max_delay: int) -> Any:
 
-    Create an asyncio.Task object that calls wait_random with a specified maximum delay.
+    Create an asyncio.Task object that calls wait_random with a specified
+    maximum delay.
 
     Args:
-    - max_delay: an integer representing the maximum delay value for the wait_random call.
+    - max_delay: an integer representing the maximum delay value for the
+    wait_random call.
 
     Returns:
-    - An asyncio.Task object created by the asyncio event loop, representing the asynchronous execution of wait_random with the specified max_delay value.
+    - An asyncio.Task object created by the asyncio event loop, representing
+    the asynchronous execution of wait_random with the specified max_delay
+    value.
 """
     loop = asyncio.get_event_loop()
     return loop.create_task(wait_random(max_delay))
--- python_async_function/4-tasks.py
+++ python_async_function/4-tasks.py
@@ -12,16 +12,17 @@ task_wait_random = __import__('3-tasks').task_wait_random
 
 async def task_wait_n(n: int, max_delay: int) -> List[float]:
     """
-    Spawn task_wait_random n times with max_delay and return a list of the resulting delays.
+    Spawn task_wait_random n times with max_delay and return a list of the
+    resulting delays.
 
     Args:
     - n: The number of times to call task_wait_random.
     - max_delay: The maximum value for each call to task_wait_random.
 
     Returns:
-    - A list of floats, representing the resulting delays of each call to task_wait_random.
+    - A list of floats, representing the resulting delays of each call to
+    task_wait_random.
     """
-
 
     coroutines = [task_wait_random(max_delay) for _ in range(n)]
     delays = []
