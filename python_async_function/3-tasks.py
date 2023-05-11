aac6d6ee21507f1225e9d8d72278e2456f1b078d
nathanhall762 <4943@holbertonstudents.com>
2023-05-11 10:25:53 -0500

    fix task 3

--- python_async_function/3-tasks.py
+++ python_async_function/3-tasks.py
@@ -11,9 +11,8 @@ from typing import Any
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
-def task_wait_random(max_delay: int) -> Any:
-    """task_wait_random(max_delay: int) -> Any:
-
+def task_wait_random(max_delay: int) -> _asyncio.Task:
+    """
     Create an asyncio.Task object that calls wait_random with a specified
     maximum delay.
 
