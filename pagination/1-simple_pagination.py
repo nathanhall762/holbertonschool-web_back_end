e4b2faa1a28dc3c035cb99dd40516703b6a27293
nathanhall762 <4943@holbertonstudents.com>
2023-05-17 14:19:11 -0500

    task 1

--- pagination/1-simple_pagination.py
+++ pagination/1-simple_pagination.py
@@ -30,15 +30,12 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
-        pass
-
-    def get_page(page: int = 1, page_size: int = 10) -> List[List]:
         """ get page of dataset """
         assert isinstance(page, int) and page > 0
         assert isinstance(page_size, int) and page_size > 0
 
         dataset = self.dataset()
-        start, end = index_range(page, page_size, len(dataset))
+        start, end = index_range(page, page_size)
 
         if start > len(dataset) or end > len(dataset):
             return []
