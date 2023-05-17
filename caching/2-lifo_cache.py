3c1093ff35499d29cacf7669926ad6ce3ef309a8
nathanhall762 <4943@holbertonstudents.com>
2023-05-17 11:37:14 -0500

    task 2

--- caching/2-lifo_cache.py
+++ caching/2-lifo_cache.py
@@ -25,6 +25,7 @@ class LIFOCache(BaseCaching):
             None.
         """
         super().__init__()
+        self.key_order = []
 
     def put(self, key, item):
         """
@@ -42,19 +43,20 @@ class LIFOCache(BaseCaching):
             - If the number of items in self.cache_data exceeds
             BaseCaching.MAX_ITEMS,
             the last item put in the cache is discarded (LIFO).
-            A "DISCARD: <key>" message is printed indicating the discarded
-            key.
+            A "DISCARD: <key>" message is printed indicating the
+            discarded key.
         """
         if key is None or item is None:
             return
 
         if len(self.cache_data) >= self.MAX_ITEMS:
             # Discard the last item put in cache (LIFO)
-            last_key = next(reversed(self.cache_data))
+            last_key = self.key_order.pop()
             del self.cache_data[last_key]
             print(f"DISCARD: {last_key}")
 
         self.cache_data[key] = item
+        self.key_order.append(key)
 
     def get(self, key):
         """
