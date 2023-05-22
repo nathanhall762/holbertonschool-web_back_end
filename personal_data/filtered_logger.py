a55545ba5d9328105045f123439d275f3307cd86
nathanhall762 <4943@holbertonstudents.com>
2023-05-22 14:04:39 -0500

    added docstring

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -6,6 +6,22 @@ import re
 
 
 def filter_datum(fields, redaction, message, separator):
-    """ filters a datum """
+    """
+    Obfuscates specified fields in a log message.
+
+    Args:
+        fields (list of str): A list of strings
+        representing the fields to obfuscate.
+        redaction (str): A string representing the
+        value to use for obfuscation.
+        message (str): A string representing the log
+        line to process.
+        separator (str): A string representing the
+        character that separates the fields in the log line.
+
+    Returns:
+        str: The obfuscated log message with the specified
+        fields replaced by the redaction string.
+    """
     return re.sub(r'(' + '|'.join(fields) + r')=[^{}]+'.format(separator),
                   r'\1={}'.format(redaction), message)
