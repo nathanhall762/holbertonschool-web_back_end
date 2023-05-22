0339e7d0f0741305efda1b1bd783fdc57e2536b1
nathanhall762 <4943@holbertonstudents.com>
2023-05-22 14:40:06 -0500

    task 2

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -7,6 +7,30 @@ from typing import List
 import logging
 
 
+PII_FIELDS = ("name", "email", "phone", "ssn", "password")
+
+
+def get_logger() -> logging.Logger:
+    """
+    Create and configure a logging.Logger object.
+
+    Returns:
+        logging.Logger: The configured logger named "user_data".
+
+    """
+    logger = logging.getLogger("user_data")
+    logger.setLevel(logging.INFO)
+    logger.propagate = False
+
+    stream_handler = logging.StreamHandler()
+    formatter = RedactingFormatter(PII_FIELDS)
+    stream_handler.setFormatter(formatter)
+
+    logger.addHandler(stream_handler)
+
+    return logger
+
+
 class RedactingFormatter(logging.Formatter):
     """Redacting Formatter class
 
