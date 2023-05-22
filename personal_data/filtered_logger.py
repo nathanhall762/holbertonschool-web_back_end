ddcc599e48a5885346acd23c2cfd5c09e17ffa25
nathanhall762 <4943@holbertonstudents.com>
2023-05-22 14:23:48 -0500

    added annotations

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -5,23 +5,21 @@ does things related to logging i think
 import re
 
 
-def filter_datum(fields, redaction, message, separator):
+def filter_datum(fields: List[str], redaction: str, message: str,
+                 separator: str) -> str:
     """
     Obfuscates specified fields in a log message.
 
     Args:
-        fields (list of str): A list of strings
-        representing the fields to obfuscate.
-        redaction (str): A string representing the
-        value to use for obfuscation.
-        message (str): A string representing the log
-        line to process.
-        separator (str): A string representing the
-        character that separates the fields in the log line.
+        fields: A list of strings representing the fields to obfuscate.
+        redaction: A string representing the value to use for obfuscation.
+        message: A string representing the log line to process.
+        separator: A string representing the character that separates the
+        fields in the log line.
 
     Returns:
-        str: The obfuscated log message with the specified
-        fields replaced by the redaction string.
+        The obfuscated log message with the specified fields replaced by the
+        redaction string.
     """
     return re.sub(r'(' + '|'.join(fields) + r')=[^{}]+'.format(separator),
                   r'\1={}'.format(redaction), message)
