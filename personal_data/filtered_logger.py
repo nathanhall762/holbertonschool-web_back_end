7ba738e7642ff219aabe3afb99a2c372a3b7f22a
nathanhall762 <4943@holbertonstudents.com>
2023-05-22 14:29:43 -0500

    task 1

--- personal_data/filtered_logger.py
+++ personal_data/filtered_logger.py
@@ -4,6 +4,25 @@ does things related to logging i think
 """
 import re
 from typing import List
+import logging
+
+
+class RedactingFormatter(logging.Formatter):
+    """Redacting Formatter class"""
+
+    REDACTION: str = "***"
+    FORMAT: str = "[HOLBERTON] %(name)s %(levelname)s %(asctime)-15s: \
+        %(message)s"
+    SEPARATOR: str = ";"
+
+    def __init__(self, fields: List[str]):
+        super(RedactingFormatter, self).__init__(self.FORMAT)
+        self.fields = fields
+
+    def format(self, record: logging.LogRecord) -> str:
+        log_message: str = super().format(record)
+        return filter_datum(self.fields, self.REDACTION, log_message,
+                            self.SEPARATOR)
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
