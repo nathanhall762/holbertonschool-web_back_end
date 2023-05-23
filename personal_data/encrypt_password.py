92805086e7ac7d54173a6b73411175378e6be3ed
nathanhall762 <4943@holbertonstudents.com>
2023-05-23 11:36:00 -0500

    task 6

--- personal_data/encrypt_password.py
+++ personal_data/encrypt_password.py
@@ -19,3 +19,19 @@ def hash_password(password: str) -> bytes:
     salt = bcrypt.gensalt()
     hashed_password = bcrypt.hashpw(password.encode(), salt)
     return hashed_password
+
+
+def is_valid(hashed_password: bytes, password: str) -> bool:
+    """
+    Validates whether the provided password matches the hashed password.
+
+    Args:
+        hashed_password (bytes): The hashed password as a byte string.
+        password (str): The password to be validated.
+
+    Returns:
+        bool: True if the password matches the hashed password, False
+        otherwise.
+
+    """
+    return bcrypt.checkpw(password.encode(), hashed_password)
