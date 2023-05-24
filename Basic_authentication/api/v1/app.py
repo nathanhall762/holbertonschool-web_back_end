01d05af53f583435f8bb4cefa94f378663d63cb9
nathanhall762 <4943@holbertonstudents.com>
2023-05-24 11:14:47 -0500

    before_request added

--- Basic_authentication/api/v1/app.py
+++ Basic_authentication/api/v1/app.py
@@ -8,10 +8,26 @@ from flask import Flask, jsonify, abort, request
 from flask_cors import CORS, cross_origin
 import os
 
+from api.v1.auth.auth import Auth
 
 app = Flask(__name__)
 app.register_blueprint(app_views)
 CORS(app, resources={r"/api/v1/*": {"origins": "*"}})
+
+auth = Auth()
+
+
+@app.before_request
+def before_request_handler():
+    if request.path not in ['/api/v1/status/',
+                            '/api/v1/unauthorized/',
+                            '/api/v1/forbidden/']:
+        if auth is None:
+            return
+        if auth.authorization_header(request) is None:
+            abort(401)
+        if auth.current_user(request) is None:
+            abort(403)
 
 
 @app.errorhandler(401)
@@ -30,30 +46,3 @@ def forbidden(error) -> str:
 def not_found(error) -> str:
     """Not found handler"""
     return jsonify({"error": "Not found"}), 404
-
-
-@app.before_request
-def before_request():
-    """Before request handler"""
-    auth = None  # Replace with your auth instance
-
-    if auth is None:
-        return
-
-    allowed_paths = [
-        '/api/v1/status/',
-        '/api/v1/unauthorized/',
-        '/api/v1/forbidden/'
-    ]
-
-    if request.path not in allowed_paths:
-        if auth.authorization_header(request) is None:
-            abort(401)
-        if auth.current_user(request) is None:
-            abort(403)
-
-
-if __name__ == "__main__":
-    host = getenv("API_HOST", "0.0.0.0")
-    port = getenv("API_PORT", "5000")
-    app.run(host=host, port=port)
