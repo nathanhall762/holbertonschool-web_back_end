fd148472924bcb21276c5b5f528189476f5a5280
nathanhall762 <4943@holbertonstudents.com>
2023-05-29 13:18:47 -0500

    task 6

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -15,6 +15,21 @@ class SessionAuth(Auth):
     def __init__(self):
         pass
 
+    def current_user(self, request=None):
+        """Returns a User instance based on a session cookie value"""
+        if request is None:
+            return None
+
+        session_id = self.session_cookie(request)
+        if session_id is None:
+            return None
+
+        user_id = self.user_id_for_session_id(session_id)
+        if user_id is None:
+            return None
+
+        return User.get(user_id)
+
     def create_session(self, user_id: str = None) -> str:
         """Creates a Session ID for a user_id"""
         if user_id is None or not isinstance(user_id, str):
