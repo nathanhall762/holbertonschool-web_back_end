911bb52a35d5151b50b6be5523f940debfc2eeab
nathanhall762 <4943@holbertonstudents.com>
2023-05-29 13:41:07 -0500

    added char limit

--- user_authentication_service/main.py
+++ user_authentication_service/main.py
@@ -8,11 +8,3 @@ print(User.__tablename__)
 
 for column in User.__table__.columns:
     print("{}: {}".format(column, column.type))
-
-bob@dylan: ~$ python3 main.py
-users
-users.id: INTEGER
-users.email: VARCHAR(250)
-users.hashed_password: VARCHAR(250)
-users.session_id: VARCHAR(250)
-users.reset_token: VARCHAR(250)
