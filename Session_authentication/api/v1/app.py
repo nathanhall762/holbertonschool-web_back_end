a4876544b1fa307252ac7ea8430c71887a65bcae
nathanhall762 <4943@holbertonstudents.com>
2023-05-29 13:10:32 -0500

    task 4

--- Session_authentication/api/v1/app.py
+++ Session_authentication/api/v1/app.py
@@ -55,11 +55,14 @@ def unauthorized(error) -> str:
 def before_request():
     """Filters requests to error handlers
     """
-    path_list = ['/api/v1/status/',
-                 '/api/v1/unauthorized',
-                 '/api/v1/forbidden']
+    path_list = [
+        '/api/v1/status/',
+        '/api/v1/unauthorized',
+        '/api/v1/forbidden',
+        '/api/v1/auth_session/login/'  # Added this path to the excluded list
+    ]
     if auth and auth.require_auth(request.path, path_list):
-        if auth.authorization_header(request) is None:
+        if auth.authorization_header(request) is None and auth.session_cookie(request) is None:
             abort(401)
         request.current_user = auth.current_user(request)
         if auth.current_user(request) is None:
