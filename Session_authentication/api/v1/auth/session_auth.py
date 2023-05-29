c634ad91f7da8f24810f6f32cf52a55f477fe134
nathanhall762 <4943@holbertonstudents.com>
2023-05-29 12:47:50 -0500

    task 2

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -6,14 +6,32 @@ from api.v1.auth.auth import Auth
 from typing import List, TypeVar
 
 
+import uuid
+from api.v1.auth.auth import Auth
+from typing import List, TypeVar
+
+
 class SessionAuth(Auth):
-    """ Session authentication
-    """
+    """Session authentication"""
+
+    user_id_by_session_id = {}
 
     def __init__(self):
         pass
 
-    def current_user(self, request=None) -> TypeVar('User'):
-        """Returns a User object
-        """
-        return None
+    def create_session(self, user_id: str = None) -> str:
+        """Creates a Session ID for a user_id"""
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+
+User = TypeVar('User')
+
+
+def current_user(self, request=None) -> User:
+    """Returns a User object"""
+    return None
