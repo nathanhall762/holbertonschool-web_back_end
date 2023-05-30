58de821e32a00836d8f4a449ded5712ce100467e
nathanhall762 <4943@holbertonstudents.com>
2023-05-30 13:43:54 -0500

    task 6

--- Session_authentication/api/v1/auth/session_auth.py
+++ Session_authentication/api/v1/auth/session_auth.py
@@ -31,20 +31,14 @@ class SessionAuth(Auth):
         return User.get(user_id)
 
     def create_session(self, user_id: str = None) -> str:
-        """Creates a Session ID for a user_id"""
+        """Creates a Session ID for a `user_id`"""
         if user_id is None or not isinstance(user_id, str):
             return None
-
+        print(f"User ID: {user_id}")
         session_id = str(uuid.uuid4())
+        print(f"Generated Session ID: {session_id}")
         self.user_id_by_session_id[session_id] = user_id
         return session_id
-
-    def user_id_for_session_id(self, session_id: str = None) -> str:
-        """Returns a User ID based on a Session ID"""
-        if session_id is None or not isinstance(session_id, str):
-            return None
-
-        return self.user_id_by_session_id.get(session_id)
 
 
 User = TypeVar('User')
