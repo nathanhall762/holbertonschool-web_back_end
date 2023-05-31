db9bef9232382ffbf3a8ee607c587eef625c1fde
nathanhall762 <4943@holbertonstudents.com>
2023-05-31 13:17:45 -0500

    task 11

--- user_authentication_service/app.py
+++ user_authentication_service/app.py
@@ -23,5 +23,22 @@ def register_user():
         return jsonify({"message": str(e)}), 400
 
 
+@app.route('/sessions', methods=['POST'])
+def login():
+    email = request.form.get('email')
+    password = request.form.get('password')
+
+    if AUTH.valid_login(email, password):
+        session_id = AUTH.create_session(email)
+        if session_id:
+            response = jsonify({'email': email, 'message': 'logged in'})
+            response.set_cookie('session_id', session_id)
+            return response
+        else:
+            abort(500)  # Failed to create a session
+    else:
+        abort(401)  # Incorrect login information
+
+
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port="5000")
