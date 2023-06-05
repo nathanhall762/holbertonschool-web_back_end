76d5949733c07d75e90c4203c67eeddc76d6a26c
nathanhall762 <4943@holbertonstudents.com>
2023-06-05 10:19:26 -0500

    task 2

--- Unittests_and_integration_tests/test_utils.py
+++ Unittests_and_integration_tests/test_utils.py
@@ -1,19 +1,24 @@
 #!/usr/bin/env python3
 import unittest
 from parameterized import parameterized
+from unittest.mock import patch, MagicMock
 
-from utils import access_nested_map
+from utils import get_json
 
 
-class TestAccessNestedMap(unittest.TestCase):
+class TestGetJson(unittest.TestCase):
 
     @parameterized.expand([
-        ({}, ("a",)),
-        ({"a": 1}, ("a", "b"))
+        ("http://example.com", {"payload": True}),
+        ("http://holberton.io", {"payload": False})
     ])
-    def test_access_nested_map_exception(self, nested_map, path):
-        with self.assertRaises(KeyError) as context:
-            access_nested_map(nested_map, path)
+    def test_get_json(self, test_url, test_payload):
+        mock_response = MagicMock()
+        mock_response.json.return_value = test_payload
 
-        exception = context.exception
-        self.assertEqual(str(exception), f"'{path[-1]}'")
+        with patch('utils.requests.get', return_value=mock_response) \
+             as mock_get:
+            result = get_json(test_url)
+
+            mock_get.assert_called_once_with(test_url)
+            self.assertEqual(result, test_payload)
