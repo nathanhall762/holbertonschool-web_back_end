e8fbd64d996eb9d20f60511659b2dcf3613288f1
nathanhall762 <4943@holbertonstudents.com>
2023-06-06 11:32:19 -0500

    task 0 docstrings

--- i18n/0-app.py
+++ i18n/0-app.py
@@ -1,4 +1,17 @@
 #!/usr/bin/env python3
+"""
+This is a basic Flask application that displays a simple HTML page.
+
+It defines a single route at the root URL ("/") and renders the index.html
+template.
+
+Usage:
+    Run this script to start the Flask application:
+    $ python3 0-app.py
+
+Author:
+    Your Name
+"""
 from flask import Flask, render_template
 
 app = Flask(__name__)
@@ -6,6 +19,12 @@ app = Flask(__name__)
 
 @app.route('/')
 def index():
+    """
+    Render the index.html template.
+
+    Returns:
+        str: Rendered HTML template.
+    """
     return render_template('0-index.html')
 
 
