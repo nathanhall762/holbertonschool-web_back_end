4792e6011529437d49c9a0ecf55fa6f68fca8260
nathanhall762 <nathanhall762@gmail.com>
2023-06-23 14:49:39 -0500

    task 1

---
+++ Node_JS_basic/1-stdin.js
@@ -0,0 +1,18 @@
+// 1-stdin.js
+const readline = require('readline');
+
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+
+console.log('Welcome to Holberton School, what is your name?');
+
+rl.on('line', (name) => {
+  if (name === '') {
+    console.log('This important software is now closing');
+    rl.close();
+  } else {
+    console.log(`Your name is: ${name}`);
+  }
+});
