550bb053adc75878eb26e3b2a55dfc9a2dca697c
nathanhall762 <nathanhall762@gmail.com>
2023-06-26 13:59:11 -0500

    task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -1,20 +1,15 @@
-// 1-stdin.js
-const readline = require('readline');
+// 1. Write a program that takes input from the user using the process.stdin.on() method.
 
-const rl = readline.createInterface({
-  input: process.stdin,
-  output: process.stdout,
-});
+process.stdout.write('Welcome to Holberton School, what is your name?\n');
 
-console.log('Welcome to Holberton School, what is your name?');
-
-rl.on('line', (name) => {
-  if (name === '') {
-    console.log('Your name is: undefined\n');
-    rl.close();
+process.stdin.once('data', (data) => {
+  const name = data.toString().trim();
+  if (name !== '') {
+    process.stdout.write(`Your name is: ${name}\n`);
   } else {
-    console.log(`Your name is: ${name}`);
+    process.stdout.write('Your name is: undefined\n');
   }
-  console.log('This important software is now closing');
-  process.exit(0);
+
+  process.stdout.write('This important software is now closing\n');
+  process.exit();
 });
