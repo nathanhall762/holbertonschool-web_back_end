4d1c40cf8ae48e9179c0035aa833451f8ec0b95b
nathanhall762 <nathanhall762@gmail.com>
2023-06-26 14:43:37 -0500

    task 3

---
+++ Node_JS_basic/4-http.js
@@ -0,0 +1,12 @@
+// 4.
+const http = require('http');
+
+const app = http.createServer((req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'text/plain');
+  res.end('Hello Holberton School!\n');
+});
+
+app.listen(1245);
+
+module.exports = app;
