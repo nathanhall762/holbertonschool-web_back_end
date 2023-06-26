e58e9c0c82afdfe7decdf7af53a26cf2fdf7e1a2
nathanhall762 <nathanhall762@gmail.com>
2023-06-26 13:56:48 -0500

    task 1

--- Node_JS_basic/1-stdin.js
+++ Node_JS_basic/1-stdin.js
@@ -10,19 +10,11 @@ console.log('Welcome to Holberton School, what is your name?');
 
 rl.on('line', (name) => {
   if (name === '') {
-    console.log('This important software is now closing');
+    console.log('Your name is: undefined\n');
     rl.close();
   } else {
     console.log(`Your name is: ${name}`);
   }
-});
-
-rl.on('SIGINT', () => {
-  console.log('This important software is now closing');
-  rl.close();
-});
-
-rl.on('close', () => {
   console.log('This important software is now closing');
   process.exit(0);
 });
